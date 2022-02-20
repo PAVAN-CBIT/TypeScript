9c202b27de4a720eb5476dbaedb117b8d0972b39
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2022-02-20 15:40:09 +0530

    Create Data_type.ts

---
+++ Data_type.ts
@@ -0,0 +1,5 @@
+let ProductName:string = "Samsung TV";
+let Price:number = 45000.55;
+let Stock:boolean = true;
+
+console.log(`Name=${ProductName}\nPrice=${Price}\nStock=${(Stock==true)?'Available':'Out of Stock'}`);
