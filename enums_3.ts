9790275682792784dcb20afb049be650136d3274
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2022-02-22 10:56:50 +0530

    Create enums_3.ts

---
+++ enums_3.ts
@@ -0,0 +1,12 @@
+enum Route
+{
+    Electronics,                                
+    Home = 'http://localhost/app/home',
+    Contact = 'http://127.0.0.1/app/contact?address',
+    Fashion = 200,
+    Tesla='https://www.tesla.com/',
+}
+console.log(Route.Tesla);
+console.log(Route.Home);
+console.log(Route.Contact);
+console.log(Route.Fashion);
